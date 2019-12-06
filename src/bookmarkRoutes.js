e28a2f51dbd112e2626d2d0425e24718a98d852f
Daniel Kent <Kentpercussion@gmail.com>
2019-12-06 12:13:43 -0700

    fixed post bug

--- src/bookmarkRoutes.js
+++ src/bookmarkRoutes.js
@@ -13,24 +13,26 @@ const logger = winston.createLogger({
   ]
 });
 
+bookmarkRoutes.use(express.json());
+
 bookmarkRoutes.route('/bookmarks')
   .get((req, res) => {
     res.json(bookmarks);
   })
 
-  .post ((req,res) => {
-  const { title, content} = req.body;
+  .post ((req, res) => {
+    const {title, content} = req.body;
     if (!title) {
-      logger.error(`Title is required`);
+      logger.error('Title is required');
       return res
-      .status(400)
-      .send('Invalid data');
+        .status(400)
+        .send('Invalid data');
     }
     if (!content) {
-      logger.error(`Content is required`);
+      logger.error('Content is required');
       return res
-      .status(400)
-      .send('Invalid data');
+        .status(400)
+        .send('Invalid data');
     }
     const id = uuid();
 
@@ -44,9 +46,9 @@ bookmarkRoutes.route('/bookmarks')
 
     logger.info(`Bookmark with id ${id} created`);
     res
-    .status(201)
-    .location(`http://localhost:8000/bookmarks/${id}`)
-    .json(bookmark);
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${id}`)
+      .json(bookmark);
   })
 
 
@@ -64,21 +66,21 @@ bookmarkRoutes.route('/bookmarks/:bookmarkId')
     res.json(bookmark);
   })
   .delete((req, res) => {
-   const id = request.params.id  
-   const bookmarkIndex = bookmarks.findIndex(bm => bm.id == id);
+    const id = req.params.id  
+    const bookmarkIndex = bookmarks.findIndex(bm => bm.id == id);
 
-   if (bookmarkIndex === -1) {
-     logger.error(`List with id ${id} not found.`);
-     return res
-      .status(404)
-      .send('Not found');
-   }
-   bookmarks.splice(bookmarkIndex, 1);
+    if (bookmarkIndex === -1) {
+      logger.error(`List with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Not found');
+    }
+    bookmarks.splice(bookmarkIndex, 1);
 
-   logger.info(`List with id ${id} deleted.`);
-   res
-    .status(204)
-    .end();
+    logger.info(`List with id ${id} deleted.`);
+    res
+      .status(204)
+      .end();
   });
 
 module.exports = bookmarkRoutes;
